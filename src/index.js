ad64c704f97f84a94f5b89405ba04e024477e443
Павел Мочалов <78229095+010MochalovPA@users.noreply.github.com>
2021-03-06 15:04:25 +0300

    feat: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function toReadable (number) {
-  
+    const numbersWords = {
+        'ones': ['zero','one','two','three','four','five','six','seven','eight','nine'],
+        'secondTen': ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'],
+        'tens': [,'ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+    }
+    let numberOfDigits = String(number).length;
+    switch (numberOfDigits) {
+        case 1:
+            return (numbersWords.ones[number]);
+            break;
+        case 2:
+            if (number < 20){
+                return (numbersWords.secondTen[number-10]);
+            } else if (number%10 === 0) {
+                return (numbersWords.tens[number/10]);
+            } else {
+                return (numbersWords.tens[Math.trunc(number/10)]
+                            + " "
+                            + numbersWords.ones[number-Math.trunc(number/10)*10]);
+            }
+            break;
+        case 3:
+            if (number%100 === 0){
+                return (numbersWords.ones[Math.trunc(number/100)]
+                            + " "
+                            + "hundred");
+            } else if (number%10 === 0){
+                return (numbersWords.ones[Math.trunc(number/100)]
+                            + " "
+                            + "hundred"
+                            + " "
+                            + numbersWords.tens[(number%100)/10]);
+            } else if (number - Math.trunc(number/100)*100 < 10) {
+                return (numbersWords.ones[Math.trunc(number/100)]
+                            + " "
+                            + "hundred"
+                            + " "
+                            + numbersWords.ones[number - Math.trunc(number/100)*100]);
+            } else if (number - Math.trunc(number/100)*100 < 20) {
+                return (numbersWords.ones[Math.trunc(number/100)]
+                            + " "
+                            + "hundred"
+                            + " "
+                            + numbersWords.secondTen[(number - Math.trunc(number/100)*100)-10]);
+            } else {
+                return (numbersWords.ones[Math.trunc(number/100)]
+                            + " "
+                            + "hundred"
+                            + " "
+                            + numbersWords.tens[(number - Math.trunc(number/100)*100 - (number - Math.trunc(number/10)*10))/10]
+                            + " "
+                            + numbersWords.ones[number - Math.trunc(number/10)*10]);
+            }
+            break; 
+    }
 }
